0b3e79f617efb4e1a1918478127e739b6df2260e
whitedot <whitedot@naver.com>
2026-06-25 01:06:20 +0900

    fix: 팝업 쿠폰 CTA 업데이트 재실행 보호 #370

--- modules/popup_layer/updates/2026.06.002.sql
+++ modules/popup_layer/updates/2026.06.002.sql
@@ -1,5 +1,18 @@
-ALTER TABLE sr_popup_layers
-    ADD COLUMN coupon_claim_campaign_key VARCHAR(60) NOT NULL DEFAULT '' AFTER body_format;
+SET @schema_has_popup_layers_coupon_claim_campaign_key = (
+    SELECT COUNT(*)
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_SCHEMA = DATABASE()
+      AND TABLE_NAME = '{{SR_TABLE_PREFIX}}popup_layers'
+      AND COLUMN_NAME = 'coupon_claim_campaign_key'
+);
+SET @schema_sql = IF(
+    @schema_has_popup_layers_coupon_claim_campaign_key = 0,
+    'ALTER TABLE {{SR_TABLE_PREFIX}}popup_layers ADD COLUMN coupon_claim_campaign_key VARCHAR(60) NOT NULL DEFAULT '''' AFTER body_format',
+    'DO 0'
+);
+PREPARE schema_stmt FROM @schema_sql;
+EXECUTE schema_stmt;
+DEALLOCATE PREPARE schema_stmt;
 
 UPDATE sr_modules
 SET version = '2026.06.002',
